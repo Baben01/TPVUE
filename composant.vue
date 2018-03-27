7dc3d73e88303c6534c6b96ecb0dff2913da8548
Normand Benjamin <benjamin.normand@etu.u-bordeaux.fr>
2018-03-27 12:09:09 +0200

    TAMER

---
+++ composant.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>TEEEST</p>
+    <li v-for="personne in personnes">{{personne.nom}} {{personne.prenom}}</li>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            {
+                return {
+                    personnes: [
+                        {nom: "Durand", prenom: "Jacques"},
+                        {nom: "Dupont", prenom: "Albert"},
+                        {nom: "Martin", prenom: "Denis"},
+                    ]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ js.js
@@ -0,0 +1,8 @@
+
+import test from 'app'
+
+var app = new Vue({
+    el: '#app',
+    template: "<app/>",
+    components: { test }
+});
